c10a4bf51be19119346b5586e09af5ee97e2f72c
Andrew Couch <andy@couchand.com>
2013-04-02 13:20:15 -0400

    cuke sauce: integrate with plugin and log build info

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,18 @@
 require 'rubygems'
 require 'selenium-webdriver'
 
-$driver = Selenium::WebDriver.for :remote, :url => 'http://couchand:ba998d45-e4c9-496b-add9-b4670ceed1a1@ondemand.saucelabs.com:80/wd/hub'
+$capabilities = Selenium::WebDriver::Capabilities.new {
+    :browser_name => ENV["SELENIUM_BROWSER"],
+    :version      => ENV["SELENIUM_VERSION"],
+    :platform     => ENV["SELENIUM_PLATFORM"],
+    :name         => "#{ENV["JOB_NAME"]} ##{ENV["BUILD_NUMBER"]}",
+    :build        => ENV["BUILD_TAG"],
+    :tags         => [ ENV["JOB_NAME"], "r#{ENV["SVN_REVISION"]}" ]
+}
+
+$driver = Selenium::WebDriver.for :remote,
+    :url => "http://#{ENV["SAUCE_USER_NAME"]}:#{ENV["SAUCE_API_KEY"]}@ondemand.saucelabs.com:80/wd/hub",
+    :desired_capabilities => $capabilities
 
 at_exit do
   $driver.close
