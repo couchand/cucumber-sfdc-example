710519783abf2ec416296b651397713e379e6fd7
Andrew Couch <andy@couchand.com>
2013-04-02 13:23:11 -0400

    ruby

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,14 +1,14 @@
 require 'rubygems'
 require 'selenium-webdriver'
 
-$capabilities = Selenium::WebDriver::Capabilities.new {
+$capabilities = Selenium::WebDriver::Capabilities.new({
     :browser_name => ENV["SELENIUM_BROWSER"],
     :version      => ENV["SELENIUM_VERSION"],
     :platform     => ENV["SELENIUM_PLATFORM"],
     :name         => "#{ENV["JOB_NAME"]} ##{ENV["BUILD_NUMBER"]}",
     :build        => ENV["BUILD_TAG"],
     :tags         => [ ENV["JOB_NAME"], "r#{ENV["SVN_REVISION"]}" ]
-}
+})
 
 $driver = Selenium::WebDriver.for :remote,
     :url => "http://#{ENV["SAUCE_USER_NAME"]}:#{ENV["SAUCE_API_KEY"]}@ondemand.saucelabs.com:80/wd/hub",
